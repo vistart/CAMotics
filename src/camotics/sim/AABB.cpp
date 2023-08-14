c375d20f6126e329dcabdcf46492adbdcfc0059d
Zhao vistart <i@vistart.me>
2023-08-14 20:57:21 +0800

    补充注释。

--- src/camotics/sim/AABB.cpp
+++ src/camotics/sim/AABB.cpp
@@ -20,6 +20,8 @@
 
 #include "AABB.h"
 
+// 源代码见 https://github.com/CauldronDevelopmentLLC/cbang/blob/master/src/cbang/Zap.h
+// 只包括一个宏定义方法 zap(x)，作用为删除对象并将指针指向0（空地址）。
 #include <cbang/Zap.h>
 
 #include <algorithm>
@@ -30,53 +32,53 @@ using namespace CAMotics;
 
 
 /// NOTE: Expects @param nodes to be a link list along the left child
-AABB::AABB(AABB *nodes) : left(0), right(0), move(0) {
-  if (!nodes) return;
+AABB::AABB(AABB *nodes) : left(0), right(0), move(0) { // 构造函数，并将左右邻连接起来。左右邻默认为空指针。
+  if (!nodes) return; // 若指定的节点组不存在，则直接返回，即构造不含左右邻的节点。
 
   // Compute bounds
-  unsigned count = 0;
+  unsigned count = 0; // 计算边界。
   Vector3D cutV;
-  for (AABB *it = nodes; it; it = it->left) {
-    if (it->right) THROW("Unexpected right-hand AABB node");
-    add(*it);
-    count++;
-    cutV += it->getMax() + it->getMin();
+  for (AABB *it = nodes; it; it = it->left) { // 循环遍历每个节点：
+    if (it->right) THROW("Unexpected right-hand AABB node"); // 若存在右节点，则抛出异常，即不希望存在右节点，因为我们想将自己挨个附加为当前节点的右节点。
+    add(*it); // 添加当前节点。
+    count++; // 计数+1
+    cutV += it->getMax() + it->getMin(); // 边界就是最大值和最小值的和。
   }
 
   // Degenerate cases
-  if (count < 3) {
-    if (count == 2) right = nodes->left;
-    left = nodes->prepend(0);
-    return;
+  if (count < 3) { // 如果添加的节点数不到3个。
+    if (count == 2) right = nodes->left; // 若添加了两个，则让自己的左邻作为右邻。
+    left = nodes->prepend(0); // 自己的左邻为空。
+    return; // 构造完毕。
   }
-
+  // 若添加的节点数大于等于3个：
   // Decide split
-  unsigned axis = getDimensions().findLargest();
-  double cut = cutV[axis] / count;
+  unsigned axis = getDimensions().findLargest(); // 找到每个维度中占用范围最大的轴。
+  double cut = cutV[axis] / count; // 确定分割数，即按最大轴除以添加的节点数。
 
-  // Partition nodes
+  // Partition nodes 分块节点
   AABB *lessThan = 0;
   AABB *greaterThan = 0;
   unsigned lessCount = 0;
   unsigned greaterCount = 0;
 
-  for (AABB *it = nodes; it;) {
+  for (AABB *it = nodes; it;) { // 挨个遍历每个节点。
     AABB *next = it->left;
-    bool less = it->getMax()[axis] + it->getMin()[axis] < cut;
+    bool less = it->getMax()[axis] + it->getMin()[axis] < cut; // 判断当前节点的上下限值是否小于分快数。
 
-    if (less) {lessThan = it->prepend(lessThan); lessCount++;}
-    else {greaterThan = it->prepend(greaterThan); greaterCount++;}
+    if (less) {lessThan = it->prepend(lessThan); lessCount++;} // 若是，将小于自己的附加为自己的左节点。
+    else {greaterThan = it->prepend(greaterThan); greaterCount++;} // 否则，将大于自己的附加为自己的右节点。
 
-    it = next;
+    it = next; // 遍历下一个。
   }
 
   // Check for bad partition
-  if (!lessThan) lessThan = greaterThan->split(greaterCount / 2);
-  if (!greaterThan) greaterThan = lessThan->split(lessCount / 2);
+  if (!lessThan) lessThan = greaterThan->split(greaterCount / 2); // 若没有小于自己的，则小于自己的就是大于自己的继续对半分割。
+  if (!greaterThan) greaterThan = lessThan->split(lessCount / 2); // 若没有大于自己的，则大于自己的就是小于自己的继续对半分割。
 
   // Recur
-  left = new AABB(lessThan);
-  right = new AABB(greaterThan);
+  left = new AABB(lessThan); // 自己的左节点全部安排为小于自己的。
+  right = new AABB(greaterThan); // 自己的右节点全部安排为大于自己的。
 }
 
 
@@ -93,32 +95,32 @@ AABB *AABB::prepend(AABB *list) {
 
 
 AABB *AABB::split(unsigned count) {
-  if (!count) return this;
-  if (count != 1) return left ? left->split(count - 1) : 0;
-  AABB *tmp = left;
+  if (!count) return this; // 参数为零时表示不分割，返回自己。
+  if (count != 1) return left ? left->split(count - 1) : 0; // 参数不为1时，判断是否有左邻，若有则分割自己的左邻，分割数减1。若没有左邻，则返回空。
+  AABB *tmp = left; //若分割数为1，则返回自己的左邻，并将自己的左邻清空。
   left = 0;
   return tmp;
 }
 
 
 unsigned AABB::getTreeHeight() const {
-  return max(left ? left->getTreeHeight() : 0,
-             right ? right->getTreeHeight() : 0) + 1;
+  return max(left ? left->getTreeHeight() : 0, // 若左邻存在，则获取左邻的高度；若左邻不存在，则视为0；
+             right ? right->getTreeHeight() : 0) + 1; // 若右邻存在，则获取自己右邻的高度；若右邻不存在，则视为0.二者取大视为高度。
 }
 
 
 bool AABB::intersects(const Rectangle3D &r) {
-  if (!Rectangle3D::intersects(r)) return false;
+  if (!Rectangle3D::intersects(r)) return false; // 调用 cbang 的 立体矩形判断相交的函数，若判定为不想交，则直接返回 false。
 
-  return isLeaf() ||
+  return isLeaf() || // 若判定为相交，则再判断当前节点是否为叶节点。若是，则返回 true；若不是，则判断左节点是否存在，若存在则判断左节点是否与其相交；若是则返回 true；若不是，则判断右节点是否存在，若存在则判断右积点是否与其相交，作为最后是否相交的依据。
     (left && left->intersects(r)) || (right && right->intersects(r));
 }
 
 
 void AABB::collisions(const Vector3D &p,
                       vector<const GCode::Move *> &moves) {
-  if (!Rectangle3D::contains(p)) return;
-  if (isLeaf()) moves.push_back(move);
-  if (left) left->collisions(p, moves);
-  if (right) right->collisions(p, moves);
+  if (!Rectangle3D::contains(p)) return; // 调用 cbang 的 立体矩形判断是否包含边，若不包含，则返回。
+  if (isLeaf()) moves.push_back(move); // 若当前节点是叶节点，则将当前节点的“移动”放入组中。
+  if (left) left->collisions(p, moves); // 若左节点存在，判断左节点是否与其碰撞。
+  if (right) right->collisions(p, moves); // 若右节点存在，判断右节点是否与其碰撞。
 }
--- src/camotics/sim/AABB.h
+++ src/camotics/sim/AABB.h
@@ -22,36 +22,38 @@
 
 #include <gcode/Move.h>
 
+// 源代码见 https://github.com/CauldronDevelopmentLLC/cbang/blob/master/src/cbang/geom/Rectangle.h
 #include <cbang/geom/Rectangle.h>
 
 #include <vector>
 
 
 namespace CAMotics {
+    // AABB 矩形，继承自 cbang 的立方体类。
   class AABB : public cb::Rectangle3D {
-    AABB *left;
-    AABB *right;
-    const GCode::Move *move;
+    AABB *left; // 左邻
+    AABB *right; // 右邻
+    const GCode::Move *move; // 移动方向
 
   public:
-    AABB(AABB *nodes);
+    AABB(AABB *nodes); // 构造函数，参数为立方体节点组，亦即
     AABB(const GCode::Move *move, const cb::Rectangle3D &bbox) :
       cb::Rectangle3D(bbox), left(0), right(0), move(move) {}
-    ~AABB();
+    ~AABB(); // 析构函数，作用为删除自己的左右邻居。
 
-    const AABB *getLeft() const {return left;}
-    const AABB *getRight() const {return right;}
+    const AABB *getLeft() const {return left;} // 获取自己的左邻指针。
+    const AABB *getRight() const {return right;} // 获取自己的右邻指针。
 
-    AABB *prepend(AABB *list);
-    AABB *split(unsigned count);
+    AABB *prepend(AABB *list); // 附加一组矩形为自己的左邻。
+    AABB *split(unsigned count); // 切割自己，并返回切割后的一组矩形的指针。参数为切割的目标份数。
 
-    cb::Rectangle3D getBounds() const {return *this;}
-    const GCode::Move *getMove() const {return move;}
-    bool isLeaf() const {return move;}
-    unsigned getTreeHeight() const;
+    cb::Rectangle3D getBounds() const {return *this;} // 获得边，也就是获得自己的地址。
+    const GCode::Move *getMove() const {return move;} //
+    bool isLeaf() const {return move;} // 判断自己是否为叶节点，也即是否为分割后的最小矩形。
+    unsigned getTreeHeight() const; // 获得分割树高度。
 
-    bool intersects(const cb::Rectangle3D &r);
+    bool intersects(const cb::Rectangle3D &r); // 求解与另一个矩形是否相交。
     void collisions(const cb::Vector3D &p,
-                    std::vector<const GCode::Move *> &moves);
+                    std::vector<const GCode::Move *> &moves); // 求解与一组边是否有碰撞。
   };
 }
